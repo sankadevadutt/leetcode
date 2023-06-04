ce30c98f3f76340d5bcb79a4f4f635db7b1f6c70
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2023-06-04 11:23:54 +0530

    Time: 692 ms (12.34%), Space: 51 MB (20.69%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -6,7 +6,6 @@ class Solution {
         
         for(int i =0;i<nums.length-2;i++){
             int left = i+1,right = nums.length-1;
-            
             while(left < right){
                 int sum = nums[i]+nums[left]+nums[right];
                 if(sum == 0){
@@ -18,9 +17,6 @@ class Solution {
             }
         }
         for(List<Integer> lt2 : st) lt.add(lt2);
-        
-        
-        
         return lt;
     }
 }
