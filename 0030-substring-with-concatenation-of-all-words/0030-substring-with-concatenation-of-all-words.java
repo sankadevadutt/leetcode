674c089d4573a98f4c6fe5e9f436ced08d4c93f0
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2023-06-08 01:44:27 +0530

    Time: 335 ms (12.38%), Space: 44.3 MB (37.48%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -3,20 +3,24 @@ class Solution {
         
         List<Integer> ans = new ArrayList<>();
         List<String> input = Arrays.asList(words);
+        
         Collections.sort(input);
+        
         int len = words.length;
         int size = words[0].length();
-        
         int strlen = len*size;
         
         for(int i = 0 ; i <= s.length() - strlen ; i++){
             
             String substr = s.substring(i,i+strlen);
+            
             List<String> cmp = new ArrayList<>();
-            for(int j = 0;j<=strlen-size;j+=size){
+            
+            for(int j = 0;j<=strlen-size;j+=size) 
                 cmp.add(substr.substring(j,j+size));
-            }
+            
             Collections.sort(cmp);
+            
             if(cmp.equals(input)) ans.add(i);
             
         }
