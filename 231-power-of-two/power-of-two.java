4e0b391f780a4b3b75c9c67668b9834dde33ba84
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2024-02-19 13:40:03 -0700

    Time: 0 ms (100.00%) | Memory: 40.8 MB (56.83%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n<=0) return false;
-        String str = Integer.toBinaryString(n);
-        int ind = str.indexOf('1');
-        while(ind!=-1){
-            ind = str.indexOf('1',ind+1);
-            if(ind!=-1) return false;
-        }
-        
-        return true;
+        // String str = Integer.toBinaryString(n);
+        // int ind = str.indexOf('1');
+        // while(ind!=-1){
+        //     ind = str.indexOf('1',ind+1);
+        //     if(ind!=-1) return false;
+        // }
+        return ((n & (n-1)) == 0);
     }
 }
