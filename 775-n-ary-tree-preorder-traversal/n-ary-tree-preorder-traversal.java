6bee90306defa4d4d0696b69d2efbfa24654e39c
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2024-04-17 14:01:06 -0700

    Time: 0 ms (100.00%) | Memory: 44.5 MB (57.83%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.java
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    private List<Integer> ans = new ArrayList<>();
+    public List<Integer> preorder(Node root) {
+        helper(root);
+        return ans;
+    }
+    public void helper(Node root){
+        if(root!=null){
+            ans.add(root.val);
+            for(Node child : root.children){
+                helper(child);
+            }
+        }
+        
+    }
+}
