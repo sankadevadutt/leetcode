81cc87ca002a1e83697c506b076c8ffda3c51613
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2024-04-17 13:50:56 -0700

    Time: 12 ms (10.91%) | Memory: 44.9 MB (31.90%) - LeetSync

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.java
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.java
@@ -15,12 +15,7 @@
  */
 class Solution {
     public List<String> st = new ArrayList<>();
-    public HashMap<Integer,Character> mp = new HashMap<>();
     public String smallestFromLeaf(TreeNode root) {
-        for(int i = 0;i<26;i++){
-            mp.put(i,(char)('a'+i));
-        }
-        System.out.println(mp);
         helper(root, "");
         Collections.sort(st);
         return st.get(0);
@@ -28,10 +23,10 @@ class Solution {
 
     public void helper(TreeNode root, String cur){
         if(root.left == null && root.right == null) {
-            st.add(mp.get(root.val)+cur);
+            st.add((char)('a'+root.val)+cur);
             return;
         }
-        if(root.left!=null) helper(root.left, mp.get(root.val)+cur);
-        if(root.right!=null) helper(root.right, mp.get(root.val)+cur);
+        if(root.left!=null) helper(root.left, (char)('a'+root.val)+cur);
+        if(root.right!=null) helper(root.right, (char)('a'+root.val)+cur);
     }
 }
