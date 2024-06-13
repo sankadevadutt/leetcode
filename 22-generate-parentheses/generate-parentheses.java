5de21608e80918f6842967843f16a3317027b72a
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2024-06-12 19:41:18 -0700

    Time: 19 ms (5.48%) | Memory: 45 MB (5.67%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    
+    public List<String> generateParenthesis(int n) {
+        Set<String> ret = new HashSet<>();
+        ret.add("()");
+        ret = helper(2, ret, n);
+        return new ArrayList<>(ret);
+    
+    }
+
+    public Set<String> helper(int n, Set<String> ret, int give)
+    {
+        if(n == give+1){
+            return ret;
+        }
+        Set<String> temp = new HashSet<>();
+        for(String st : ret){
+            temp.add("()" + st);
+            temp.add(st+"()");
+            temp.add('('+st+')');
+            int ind = st.indexOf(')');
+            while(ind!=-1){
+                temp.add(st.substring(0,ind)+"()"+st.substring(ind));
+                ind = st.indexOf(')',ind+1); 
+            }
+        }
+        System.out.println(n+" : "+temp);
+        return helper(n+1,temp,give);
+    }
+}
