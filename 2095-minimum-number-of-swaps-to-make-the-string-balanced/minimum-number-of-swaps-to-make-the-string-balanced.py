a9f6f75b0216385018dbcca5d00e164bed99ea51
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2024-10-07 21:27:17 -0700

    Time: 182 ms (79.86%) | Memory: 30.3 MB (17.36%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        stack = deque()
+        unbalanced = 0
+        for i in s:
+            if i == '[':
+                stack.append(i)
+            else:
+                if stack:
+                    stack.pop()
+                else:
+                    unbalanced += 1
+        
+        return (unbalanced//2 if unbalanced %2 == 0 else (unbalanced+1)//2)
