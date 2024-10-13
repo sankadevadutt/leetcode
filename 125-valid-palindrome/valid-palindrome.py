6f82da929c357878d278dfd35514eb2fdb8a049f
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2024-10-13 16:44:50 -0700

    Time: 42 ms (73.44%) | Memory: 17.9 MB (23.61%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,5 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        lt = [i for i in s if 'a' <= i <= 'z' or '0' <= i <= '9']
+        lt = [i for i in s if i.isalnum()]
         return lt == lt[::-1]
