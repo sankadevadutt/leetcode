74535553046bfe8214e3812e95341c7121e7b598
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2024-10-14 13:48:52 -0700

    Time: 1974 ms (14.40%) | Memory: 16.8 MB (34.22%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,9 +2,20 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         l, r = 0, len(s2)
         length = len(s1)
+        arr2 = [0]*26
+        for i in s1:
+            arr2[ord(i) - ord('a')] += 1
 
         def isPerm(s: str):
-            return sorted(s) == sorted(s1) 
+            # return sorted(s) == sorted(s1)
+            arr = arr2.copy()
+
+            for i in s:
+                arr[ord(i) - ord('a')] -= 1
+            for i in arr:
+                if i != 0:
+                    return False
+            return True
 
         while(l < r):
             while(l < r and s2[l] not in s1):
@@ -14,7 +25,6 @@ class Solution:
             if l+length <= r:
                 substr = s2[l:l+length]
                 if(isPerm(substr)):
-                    print(substr)
                     return True
             else:
                 return False
