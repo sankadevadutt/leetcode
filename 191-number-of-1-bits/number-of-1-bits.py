3232850056505bc3b2a9a7e39f631a4dbdce5537
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2024-10-15 23:16:16 -0700

    Time: 29 ms (90.64%) | Memory: 16.6 MB (33.73%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,9 +1,14 @@
+from collections import Counter
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        ans = 0
+        # ans = 0
 
-        while n > 0:
-            ans += 1
-            n = n&(n-1)
+        # while n > 0:
+        #     ans += 1
+        #     n = n&(n-1)
 
-        return ans
+        # return ans
+
+        s = bin(n)[2:]
+        counter = Counter(s)
+        return counter['1']
