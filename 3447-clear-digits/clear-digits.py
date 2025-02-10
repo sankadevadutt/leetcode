240dbd339445ad625b81a51a24ea9f07d8120c8b
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2025-02-09 18:26:54 -0700

    Time: 2 ms (35.03%) | Memory: 17.8 MB (43.55%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -1,19 +1,28 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        lt = list(s)
-        left, right = 0, len(s)
+        # lt = list(s)
+        # left, right = 0, len(s)
 
-        while(left < right):
-            while(left < right and not('0'<=lt[left]<='9')): left += 1
-            if(left == right): break
-            prev = left - 1
-            while(prev >= 0 and not('a'<=lt[prev]<='z')): prev -=1
-            if (prev < 0): break
-            lt[prev] = ''
-            lt[left] = ''
-            prev -= 1
-            left += 1
-            print(prev, lt)
+        # while(left < right):
+        #     while(left < right and not('0'<=lt[left]<='9')): left += 1
+        #     if(left == right): break
+        #     prev = left - 1
+        #     while(prev >= 0 and not('a'<=lt[prev]<='z')): prev -=1
+        #     if (prev < 0): break
+        #     lt[prev] = ''
+        #     lt[left] = ''
+        #     prev -= 1
+        #     left += 1
+        #     print(prev, lt)
         
+        # return ''.join(lt)
+
+        # Using Stack
+        lt = []
+        for i in s:
+            if 'a'<=i<='z':
+                lt.append(i)
+            else:
+                lt.pop()
         return ''.join(lt)
         
