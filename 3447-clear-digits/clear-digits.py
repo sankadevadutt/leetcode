1ed0593cde46bae06de70e713ada25ba1aeb6df2
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2025-02-09 18:25:31 -0700

    Time: 15 ms (5.18%) | Memory: 18 MB (7.88%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.py
@@ -0,0 +1,19 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        lt = list(s)
+        left, right = 0, len(s)
+
+        while(left < right):
+            while(left < right and not('0'<=lt[left]<='9')): left += 1
+            if(left == right): break
+            prev = left - 1
+            while(prev >= 0 and not('a'<=lt[prev]<='z')): prev -=1
+            if (prev < 0): break
+            lt[prev] = ''
+            lt[left] = ''
+            prev -= 1
+            left += 1
+            print(prev, lt)
+        
+        return ''.join(lt)
+        
