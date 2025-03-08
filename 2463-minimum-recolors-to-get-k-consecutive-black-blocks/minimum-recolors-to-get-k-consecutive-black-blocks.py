f727b7a66aa96df0fa932e842313adcb571e24a7
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2025-03-07 23:31:56 -0700

    Time: 3 ms (31.20%) | Memory: 17.8 MB (62.29%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,7 +1,7 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
         l,r = 0, len(blocks)
-        num_blacks = num_whites = 0
+        num_whites = 0
         ans = 100
         l2 = l
 
@@ -9,17 +9,32 @@ class Solution:
             while(l2 < r and (l2-l)<k):
                 if(blocks[l2] == 'W'):
                     num_whites += 1
-                else:
-                    num_blacks += 1
                 l2 += 1
             ans = min(ans, num_whites)
             if(ans == 0): return 0
             if(blocks[l] == 'W'):
                 num_whites -= 1
-            else:
-                num_blacks -= 1
             l += 1
 
         return ans
+
+        # ans = 100
+        # num_whites = 0
+
+        # for i in range(k):
+        #     if (blocks[i] == 'W'):
+        #         num_whites += 1
+        
+        # ans = min(ans, num_whites)
+        # if(ans == 0): return 0
+        # for i in range(k,len(blocks)):
+        #     if (blocks[i - k] == 'W'):
+        #         num_whites -= 1
+        #     if (blocks[i] == 'W'):
+        #         num_whites += 1
+        #     ans = min(ans, num_whites)
+        #     if(ans == 0): return 0
+        
+        # return ans
         
 
