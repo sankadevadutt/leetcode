41538cc4494478f25a6acf5a2adeb9b921b36454
Devadutt Sanka <68847694+sankadevadutt@users.noreply.github.com>
2025-03-07 23:32:16 -0700

    Time: 3 ms (31.20%) | Memory: 17.8 MB (38.61%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -17,24 +17,3 @@ class Solution:
             l += 1
 
         return ans
-
-        # ans = 100
-        # num_whites = 0
-
-        # for i in range(k):
-        #     if (blocks[i] == 'W'):
-        #         num_whites += 1
-        
-        # ans = min(ans, num_whites)
-        # if(ans == 0): return 0
-        # for i in range(k,len(blocks)):
-        #     if (blocks[i - k] == 'W'):
-        #         num_whites -= 1
-        #     if (blocks[i] == 'W'):
-        #         num_whites += 1
-        #     ans = min(ans, num_whites)
-        #     if(ans == 0): return 0
-        
-        # return ans
-        
-
